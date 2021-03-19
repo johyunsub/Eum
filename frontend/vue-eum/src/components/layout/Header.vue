37df5493ab4849818f736e2aa8b5218278b66464
sumin416 <sumin416@naver.com>
2021-03-19 13:15:07 +0900

    Add UI Basic Frame

---
+++ frontend/vue-eum/src/components/layout/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Home</b-nav-item>
+          <b-nav-item href="/dogs">Dogs</b-nav-item>
+          <b-nav-item href="/dbti">Dbti</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+<script>
+export default {
+  name: "hearder", //이 컴포넌트의 이름 지정
+};
+</script>
---
+++ frontend/vue-eum/src/router.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from './views/Home';
+import Dogs from './views/Dogs';
+import Dbti from './views/Dbti';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: Home,
+    },
+    {
+      path: '/dogs',
+      component: Dogs,
+    },
+    {
+      path: '/dbti',
+      component: Dbti,
+    },
+  ],
+});
+
+export default router;
